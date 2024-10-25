d00bc580b92ea77568c29f1edc5a783acd771233
zarah <devvick230385@gmail.com>
2024-10-25 16:53:31 +0100

    UPDATE: improved error handling on user.controller

--- controllers/user/user.controller.js
+++ controllers/user/user.controller.js
@@ -42,10 +42,18 @@ exports.get_profile_pic = async (req, res) => {
   try {
     const userId = req.query.userId;
     const photoResponse = await process_image(userId, "stream");
-    photoResponse.data.pipe(res);
+    if (!photoResponse) {
+      res.status(500).send({
+        success: false,
+        message: "Invalid response data: Unable to pipe to response.",
+        data: {},
+      });
+      return;
+    }
+    photoResponse?.data.pipe(res);
   } catch (error) {
     console.log(error);
-    res.status(500).send("server error");
+    res.status(500).send({ success: false, message: "server error", data: {} });
   }
 };
 
@@ -74,7 +82,7 @@ exports.get_leaderboard_images = async (_, res) => {
     res.status(200).send({ success: true, message: "Fetched", data: response });
   } catch (error) {
     console.log(error);
-    res.status(500).send("server error");
+    res.status(500).send({ success: false, message: "server error", data: {} });
   }
 };
 
