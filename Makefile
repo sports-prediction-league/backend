3a80364ab7cfff3040378af8a475e1576fa69256
zarah <devvick230385@gmail.com>
2024-10-29 05:39:29 +0100

    UPDATE: updated db

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ mgprod:
 	npx sequelize-cli db:migrate --env production
 
 mgtest:
-	npx sequelize-cli db:migrate --env test
+	export NODE_TLS_REJECT_UNAUTHORIZED='0' && npx sequelize-cli db:migrate --env test
 
 mgdev:
 	npx sequelize-cli db:migrate
--- config/config.js
+++ config/config.js
@@ -3,6 +3,8 @@ const DB_USERNAME = process.env.DB_USERNAME;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_HOST = process.env.DB_HOST;
 const DB_DIALECT = process.env.DB_DIALECT;
+const PORT = process.env.PORT;
+const DB = process.env.DB;
 
 module.exports = {
   development: {
@@ -17,9 +19,17 @@ module.exports = {
     username: DB_USERNAME,
     password: DB_PASSWORD,
     database: DB_USERNAME,
+    database: DB,
     host: DB_HOST,
     dialect: DB_DIALECT,
     logging: false,
+    port: PORT,
+    sslmode: "REQUIRED",
+    logging: false,
+    ssl: true,
+    dialectOptions: {
+      ssl: true,
+    },
   },
   production: {
     username: DB_USERNAME,
